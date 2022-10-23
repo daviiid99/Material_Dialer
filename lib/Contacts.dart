51b0f42ed201ad5e9bb704a66d5d05109c4a5cd2
daviiid99 <daviiideveloper@gmail.com>
2022-10-23 11:48:52 +0200

    Move CreateContact to a new dart file and introduce return for previous screen

--- lib/Contacts.dart
+++ lib/Contacts.dart
@@ -9,98 +9,11 @@ import 'dart:convert';
 import 'dart:io';
 import 'Dialer.dart';
 import 'package:path_provider/path_provider.dart';
-import 'ManageMap.dart';
+import 'CreateContact.dart';
 
 class Contacts extends StatefulWidget{
    @override
    _ContactState createState() => _ContactState();
-}
-
-class CreateContact extends StatefulWidget {
-  @override
-  _CreateContactState createState() => _CreateContactState();
-}
-
-class _CreateContactState extends State<CreateContact>{
-
-  final phone = TextEditingController();
-  final contact = TextEditingController();
-  var mapa = ManageMap();
-
-  @override
-  void initState(){
-    mapa.readJson();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    phone.dispose();
-    contact.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      backgroundColor: Colors.greenAccent,
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: const Text("Create Contact"),
-      ),
-    body: Container(
-      child: Column(
-          children: <Widget>[
-            Text("\n"),
-            TextFormField(
-              controller: phone,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Phone',
-          ),
-        ),
-            Text("\n"),
-
-        TextFormField(
-          controller: contact,
-          decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Name',
-          ),
-      ),
-
-            Text("\n\n\n"),
-
-            TextButton.icon(
-              label: const Text(
-                "Save Contact",
-                style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black),
-              ),
-              style: TextButton.styleFrom(
-                textStyle: TextStyle(color: Colors.black),
-                backgroundColor: Colors.green,
-                shape:RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24.0),
-                ),
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Dialer()),
-                );
-                setState(() {
-                  mapa.writeJson(phone.text, contact.text );
-                });
-              },
-              icon: Icon(Icons.face_rounded, color: Colors.black,),
-            )
-  ]
-    )));
-  }
 }
 
 
