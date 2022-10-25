523aa412457f0a3464865deda8444ef6c8e508df
daviiid99 <daviiideveloper@gmail.com>
2022-10-25 13:25:26 +0200

    Add initial support for updates

--- lib/MaterialDIaler.dart
+++ lib/MaterialDIaler.dart
@@ -9,6 +9,10 @@ import 'Contacts.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:open_file/open_file.dart';
 
 class MaterialDialer extends StatefulWidget{
   @override
@@ -31,6 +35,10 @@ class _MaterialDialerState extends State<MaterialDialer>{
   String buildNumber = "";
   String current_language = "";
   Map<dynamic, dynamic> language = {};
+  late String _localPath;
+  late bool _permissionReady;
+  late TargetPlatform? platform;
+
 
   @override
   _MaterialDialerState(this.mode_counter, this.modes, this.colores, this.fonts, this.current_language, this.language);
@@ -57,13 +65,67 @@ class _MaterialDialerState extends State<MaterialDialer>{
       });
     });}
 
+  Future<bool> _checkPermission() async {
+    if (platform == TargetPlatform.android) {
+      final status = await Permission.storage.status;
+      if (status != PermissionStatus.granted) {
+        final result = await Permission.storage.request();
+        if (result == PermissionStatus.granted) {
+          return true;
+        }
+      } else {
+        return true;
+      }
+    } else {
+      return true;
+    }
+    return false;
+  }
+
+
+  Future<void> _prepareSaveDir() async {
+    _localPath = (await _findLocalPath())!;
+
+    print(_localPath);
+    final savedDir = Directory(_localPath);
+    bool hasExisted = await savedDir.exists();
+    if (!hasExisted) {
+      savedDir.create();
+    }
+  }
+
+  Future<String> _findLocalPath() async {
+    return "/sdcard/download/";
+  }
+
+  Future<void> deleteFile(File file) async {
+    try {
+      if (await file.exists()) {
+        await file.delete();
+      }
+    } catch (e) {
+      // Error in getting access to the file.
+    }
+  }
+
+  Future<String> readString(String path) async {
+    return await rootBundle.loadString(path);
+  }
+
+
   @override
   void initState(){
+    super.initState();
     checkBuild();
     options = [language[current_language]["About"]["card1"], language[current_language]["About"]["card2"], language[current_language]["About"]["card3"], language[current_language]["About"]["card4"]];
     description = [appName, version, buildNumber, packageName];
-    super.initState();
+    if (Platform.isAndroid) {
+      platform = TargetPlatform.android;
+    } else {
+      platform = TargetPlatform.iOS;
+    }
   }
+
 
   @override
   Widget build(BuildContext context){
@@ -100,12 +162,70 @@ class _MaterialDialerState extends State<MaterialDialer>{
                         setState(() {
                         });
                         }
-                      ));
+                      ),
+
+
+              );
               }
           ),
 
 
+
     ),
-    ]));}
+
+      Column(
+        children: <Widget> [
+          TextButton.icon(
+            label:  Text(
+              "Download Latest",
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black),
+            ),
+            style: TextButton.styleFrom(
+              textStyle: TextStyle(color: Colors.black),
+              backgroundColor: Colors.green,
+              shape:RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24.0),
+              ),
+            ),
+            onPressed: () async {
+              _permissionReady = await _checkPermission();
+
+              if (_permissionReady) {
+                await _prepareSaveDir();
+                try {
+                  //await Dio().download("https://raw.githubusercontent.com/daviiid99/Material_Dialer/master/version.txt",
+                  //    _localPath + "/" + "version.txt");
+                  //String ver = readString(_localPath  + "version.txt") as String;
+                  //deleteFile(File(_localPath + "/" + "version.txt"));
+
+                  //print(ver);
+                 // print(version);
+
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text("Downloading latest version\nPlease wait...")));
+                  await Dio().download("https://github.com/daviiid99/Material_Dialer/releases/download/latest/app-release.apk",
+                      _localPath + "/" + "material_dialer.apk");
+                  OpenFile.open(_localPath + "/" + "material_dialer.apk");
+                  deleteFile(File(_localPath + "/" + "material_dialer.apk"));
+
+                } catch (e) {
+                }
+
+              }
+
+
+            },
+
+            icon: Icon(Icons.download, color: Colors.black,),
+          )
+        ],
+      )
+    ],
+
+
+
+    ));}
 
 }
